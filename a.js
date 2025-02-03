a2bf165822e4d7d66ec40ea855780366d1283e43
Mian Hasnain Jabbar <141392850+Hasnain-Jabbar@users.noreply.github.com>
2025-02-03 07:37:06 +0500

    done

--- a.js
+++ a.js
@@ -1,9 +1,25 @@
 function navbar() {
   let nav = document.getElementById("nav-3");
   if (nav.style.display === "flex") {
-      nav.style.display = "none";
-    } else {
-        nav.style.display = "flex";
-    }
-    console.log(nav);
+    nav.style.display = "none";
+  } else {
+    nav.style.display = "flex";
+  }
+  console.log(nav);
+}
+
+function theme() {
+  let actColor=document.getElementById('chk').value
+  document.getElementById('top').style.backgroundColor=actColor
+  document.getElementById('logo').style.color=actColor
+  for (i = 1; i <= 12; i++) {
+    let bg = document.getElementsByClassName("bgChange")[i]
+    bg.style.backgroundColor= actColor
+    console.log(bg)
+  }
+  for (j = 1; j <= 15; j++) {
+    let clr = document.getElementsByClassName("clrChange")[j]
+    clr.style.color = actColor
+    console.log(clr)
+  }
 }
